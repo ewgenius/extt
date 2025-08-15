ad9fff7d25aa624e820687fe9f78e66a3a828105
Evgenii Khramkov <hey@ewgenius.me>
2025-08-16 08:33:55 +0900

    theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { useEffect } from "react";
 import { Button } from "./components/ui/button";
 import { Tiptap } from "./Tiptap";
+import { setTheme } from "@tauri-apps/api/app";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
+async function setAppTheme(theme?: "light" | "dark" | null) {
+  const root = document.documentElement;
+  if (theme === "light") {
+    root.classList.remove("dark");
+    root.classList.add("light");
+  } else if (theme === "dark") {
+    root.classList.remove("light");
+    root.classList.add("dark");
+  } else {
+    root.classList.remove("light");
+    root.classList.remove("dark");
+  }
+}
+
+async function init() {
+  const window = getCurrentWindow();
+  const currentTheme = await window.theme();
+  setAppTheme(currentTheme);
+
+  window.show();
+  window.onThemeChanged(({ payload: theme }) => {
+    console.log("New theme: " + theme);
+    setAppTheme(theme);
+  });
+}
 
 export const App = () => {
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className="h-full w-full flex flex-row items-stretch overflow-hidden">
       <div className="bg-base-2 h-full w-64 border-r flex flex-col">
@@ -9,8 +42,35 @@ export const App = () => {
           className="h-10 min-h-10 p-2 pt-0 flex items-center gap-1 text-sm shrink-0"
           data-tauri-drag-region
         />
-        <div className="p-3 pt-0">
+        <div className="p-3 pt-0 flex flex-col gap-2">
           sidebar
+          <div className="flex items-center gap-2">
+            <Button
+              onClick={async () => {
+                await setTheme("dark");
+                setAppTheme("dark");
+              }}
+            >
+              dark
+            </Button>
+            <Button
+              onClick={async () => {
+                await setTheme("light");
+                setAppTheme("light");
+              }}
+            >
+              light
+            </Button>
+
+            <Button
+              onClick={async () => {
+                await setTheme(null);
+                setAppTheme(null);
+              }}
+            >
+              system
+            </Button>
+          </div>
           <div>
             <Button size="xs">button</Button>
           </div>
