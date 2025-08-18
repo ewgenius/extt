3c38da86f450b1b0c14b6121b71e1dd28fc30713
Evgenii Khramkov <hey@ewgenius.me>
2025-08-18 20:02:52 +0900

    update

--- src/components/ui_old/sidebar.tsx
+++ src/components/ui_old/sidebar.tsx
@@ -5,23 +5,23 @@ import { PanelLeftIcon } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui_old/button";
+import { Input } from "@/components/ui_old/input";
+import { Separator } from "@/components/ui_old/separator";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "@/components/ui/sheet";
-import { Skeleton } from "@/components/ui/skeleton";
+} from "@/components/ui_old/sheet";
+import { Skeleton } from "@/components/ui_old/skeleton";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
+} from "@/components/ui_old/tooltip";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
