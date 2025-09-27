0a926ad30631cd435eb350e86eabb201567eb3a9
Evgenii Khramkov <hey@ewgenius.me>
2025-09-27 21:09:04 +0900

    ,

---
+++ crates/extt/src/extt.rs
@@ -0,0 +1,143 @@
+use gpui::{App, Context, FontWeight, SharedString, Window, div, prelude::*, rgb, svg};
+
+use crate::colors::BASE;
+
+fn sidebar_item(path: &str) -> impl IntoElement {
+    div()
+        .id(SharedString::from(path.to_string()))
+        .flex()
+        .gap_2()
+        .px_3()
+        .py_0p5()
+        .items_center()
+        .text_color(BASE.shade(11))
+        .cursor_pointer()
+        .hover(|this| this.bg(BASE.shade(4)))
+        .active(|this| this.bg(BASE.shade(5)))
+        .child(svg().text_color(BASE.shade(11)).size_4().path(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/assets/icons/folder.svg"
+        )))
+        .child(path.to_string())
+}
+
+pub struct AppWindow {}
+
+impl Render for AppWindow {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .justify_start()
+            .items_start()
+            .bg(BASE.shade(1))
+            .text_color(BASE.shade(12))
+            .size_full()
+            .max_w_full()
+            .text_sm()
+            .font_family("TX-02")
+            .child(
+                div()
+                    .bg(BASE.shade(2))
+                    .flex_shrink_0()
+                    .pt_8()
+                    .flex()
+                    .flex_col()
+                    .h_full()
+                    .w_48()
+                    .border_r_1()
+                    .border_color(BASE.shade(4))
+                    .child(sidebar_item("root"))
+                    .child(sidebar_item("my-note-1.md"))
+                    .child(sidebar_item("my-note-2.md"))
+                    .child(sidebar_item("my-note-3.md"))
+                    .child(sidebar_item("my-note-4.md"))
+                    .child(sidebar_item("my-note-5.md"))
+                    .child(sidebar_item("my-note-6.md"))
+            )
+            .child(
+                div()
+                    .flex_1()
+                    .flex()
+                    .flex_col()
+                    .items_start()
+                    .gap_2()
+                    .p_4()
+                    .child(div().child(concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/assets/icons/folder.svg"
+                    )))
+                    .child(div().child("/Users/evgenii/Developer/BitBroz/EXTT/extt/crates/extt/assets/icons/folder.svg"))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+                    .child(div().child("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")),
+            )
+            .child(
+                div()
+                    .flex_shrink_0()
+                    .bg(BASE.shade(2))
+                    .p_4()
+                    .h_full()
+                    .flex()
+                    .flex_col()
+                    .items_start()
+                    .content_stretch()
+                    .w_96()
+                    .border_l_1()
+                    .border_color(BASE.shade(4))
+                    .child(div().flex_1())
+                    .child(
+                        div()
+                            .h_24()
+                            .p_2()
+                            .flex_shrink_0()
+                            .border_1()
+                            .border_color(BASE.shade(4))
+                            .bg(BASE.shade(1))
+                            .w_full()
+                            .rounded_xl()
+                            .flex()
+                            .flex_col()
+                            .items_end()
+                            .justify_end()
+                            .child(
+                                div()
+                                    .flex()
+                                    .flex_col()
+                                    .items_center()
+                                    .justify_center()
+                                    .size_7()
+                                    .rounded_full()
+                                    .bg(BASE.shade(6))
+                                    .text_color(rgb(0xffffff))
+                                    .text_lg()
+                                    .child("↑"),
+                            ),
+                    ),
+            )
+    }
+}
+
+fn button(text: &str, on_click: impl Fn(&mut Window, &mut App) + 'static) -> impl IntoElement {
+    div()
+        .id(SharedString::from(text.to_string()))
+        .flex()
+        .items_center()
+        .px_2()
+        .py_1()
+        .bg(BASE.shade(3))
+        .text_sm()
+        .hover(|this| this.bg(BASE.shade(4)).border_color(BASE.shade(8)))
+        .active(|this| this.bg(BASE.shade(5)).border_color(BASE.shade(8)))
+        .border_1()
+        .border_color(BASE.shade(7))
+        .rounded_sm()
+        .cursor_pointer()
+        .child(text.to_string())
+        .on_click(move |_, window, cx| on_click(window, cx))
+}
--- crates/extt/src/main.rs
+++ crates/extt/src/main.rs
@@ -1,269 +1,73 @@
+mod colors;
+mod extt;
+
+use std::{fs, path::PathBuf};
+
+use anyhow::Result;
 use gpui::{
-    App, Application, Bounds, Context, KeyBinding, PromptButton, PromptLevel, SharedString, Timer,
-    Window, WindowBounds, WindowKind, WindowOptions, actions, div, prelude::*, px, rgb, size,
+    App, Application, AssetSource, KeyBinding, SharedString, TitlebarOptions, WindowKind,
+    WindowOptions, actions, point, prelude::*, px,
 };
 
-struct SubWindow {
-    custom_titlebar: bool,
-}
-
-fn button(text: &str, on_click: impl Fn(&mut Window, &mut App) + 'static) -> impl IntoElement {
-    div()
-        .id(SharedString::from(text.to_string()))
-        .flex_none()
-        .px_2()
-        .bg(rgb(0xf7f7f7))
-        .active(|this| this.opacity(0.85))
-        .border_1()
-        .border_color(rgb(0xe0e0e0))
-        .rounded_sm()
-        .cursor_pointer()
-        .child(text.to_string())
-        .on_click(move |_, window, cx| on_click(window, cx))
-}
-
-impl Render for SubWindow {
-    fn render(&mut self, _window: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .flex_col()
-            .bg(rgb(0xffffff))
-            .size_full()
-            .gap_2()
-            .when(self.custom_titlebar, |cx| {
-                cx.child(
-                    div()
-                        .flex()
-                        .h(px(32.))
-                        .px_4()
-                        .bg(gpui::blue())
-                        .text_color(gpui::white())
-                        .w_full()
-                        .child(
-                            div()
-                                .flex()
-                                .items_center()
-                                .justify_center()
-                                .size_full()
-                                .child("Custom Titlebar"),
-                        ),
-                )
-            })
-            .child(
-                div()
-                    .p_8()
-                    .gap_2()
-                    .child("SubWindow")
-                    .child(button("Close", |window, _| {
-                        window.remove_window();
-                    })),
-            )
-    }
-}
-
-struct WindowDemo {}
-
-impl Render for WindowDemo {
-    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
-        let window_bounds =
-            WindowBounds::Windowed(Bounds::centered(None, size(px(300.0), px(300.0)), cx));
-
-        div()
-            .p_4()
-            .flex()
-            .flex_wrap()
-            .bg(rgb(0xffffff))
-            .size_full()
-            .justify_center()
-            .content_center()
-            .gap_2()
-            .child(button("Normal", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Popup", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        window_bounds: Some(window_bounds),
-                        kind: WindowKind::PopUp,
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Custom Titlebar", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        titlebar: None,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: true,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Invisible", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        show: false,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Unmovable", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        is_movable: false,
-                        titlebar: None,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Unresizable", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        is_resizable: false,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Unminimizable", move |_, cx| {
-                cx.open_window(
-                    WindowOptions {
-                        is_minimizable: false,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |_, cx| {
-                        cx.new(|_| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Hide Application", |window, cx| {
-                cx.hide();
-
-                // Restore the application after 3 seconds
-                window
-                    .spawn(cx, async move |cx| {
-                        Timer::after(std::time::Duration::from_secs(3)).await;
-                        cx.update(|_, cx| {
-                            cx.activate(false);
-                        })
-                    })
-                    .detach();
-            }))
-            .child(button("Resize", |window, _| {
-                let content_size = window.bounds().size;
-                window.resize(size(content_size.height, content_size.width));
-            }))
-            .child(button("Prompt", |window, cx| {
-                let answer = window.prompt(
-                    PromptLevel::Info,
-                    "Are you sure?",
-                    None,
-                    &["Ok", "Cancel"],
-                    cx,
-                );
-
-                cx.spawn(async move |_| {
-                    if answer.await.unwrap() == 0 {
-                        println!("You have clicked Ok");
-                    } else {
-                        println!("You have clicked Cancel");
-                    }
-                })
-                .detach();
-            }))
-            .child(button("Prompt (non-English)", |window, cx| {
-                let answer = window.prompt(
-                    PromptLevel::Info,
-                    "Are you sure?",
-                    None,
-                    &[PromptButton::ok("确定"), PromptButton::cancel("取消")],
-                    cx,
-                );
-
-                cx.spawn(async move |_| {
-                    if answer.await.unwrap() == 0 {
-                        println!("You have clicked Ok");
-                    } else {
-                        println!("You have clicked Cancel");
-                    }
-                })
-                .detach();
-            }))
-    }
-}
+use crate::extt::AppWindow;
 
 actions!(window, [Quit]);
 
-fn main() {
-    Application::new().run(|cx: &mut App| {
-        let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
+struct Assets {
+    base: PathBuf,
+}
 
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |window, cx| {
-                cx.new(|cx| {
-                    cx.observe_window_bounds(window, move |_, window, _| {
-                        println!("Window bounds changed: {:?}", window.bounds());
+impl AssetSource for Assets {
+    fn load(&self, path: &str) -> Result<Option<std::borrow::Cow<'static, [u8]>>> {
+        fs::read(self.base.join(path))
+            .map(|data| Some(std::borrow::Cow::Owned(data)))
+            .map_err(|e| e.into())
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
+        fs::read_dir(self.base.join(path))
+            .map(|entries| {
+                entries
+                    .filter_map(|entry| {
+                        entry
+                            .ok()
+                            .and_then(|entry| entry.file_name().into_string().ok())
+                            .map(SharedString::from)
                     })
-                    .detach();
+                    .collect()
+            })
+            .map_err(|e| e.into())
+    }
+}
 
-                    WindowDemo {}
-                })
-            },
-        )
-        .unwrap();
+fn main() {
+    Application::new()
+        .with_assets(Assets {
+            base: PathBuf::from(env!("CARGO_MANIFEST_DIR")),
+        })
+        .run(|cx: &mut App| {
+            cx.open_window(
+                WindowOptions {
+                    window_bounds: None,
+                    titlebar: Some(TitlebarOptions {
+                        title: None,
+                        appears_transparent: true,
+                        traffic_light_position: Some(point(px(9.0), px(9.0))),
+                    }),
+                    kind: WindowKind::Normal,
+                    is_movable: true,
+                    window_min_size: Some(gpui::Size {
+                        width: px(1024.0),
+                        height: px(640.0),
+                    }),
+                    ..Default::default()
+                },
+                |_window, cx| cx.new(|_cx| AppWindow {}),
+            )
+            .unwrap();
 
-        cx.activate(true);
-        cx.on_action(|_: &Quit, cx| cx.quit());
-        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
-    });
+            cx.activate(true);
+            cx.on_action(|_: &Quit, cx| cx.quit());
+            cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+        });
 }
